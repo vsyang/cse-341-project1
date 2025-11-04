81d7ed025bd133c4487ab869490eb48fc611cfbc
vsyang <yang.vanessa85@gmail.com>
2025-11-03 21:28:44 -0700

    started part 2 of contacts project

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+// const bodyParser = require('body-parser');
 const mongodb = require('./data/database.js');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+// app.use(bodyParser.json())
 app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
