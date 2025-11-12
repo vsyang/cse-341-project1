aafc15b2d59309e3674e592324d06f9181b8054f
vsyang <yang.vanessa85@gmail.com>
2025-11-11 18:13:42 -0700

    error handling added

--- server.js
+++ server.js
@@ -7,15 +7,21 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 // app.use(bodyParser.json())
-app.use((req, res, next) => {
+app
+    .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
+    })
+    .use('/', require('./routes'));
+
+process.on('uncaughtException', (err, origin) => {
+    console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+    process.exit(1); //mandatory (as per the Node.js docs)
 });
 
-app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
     if (err) {
